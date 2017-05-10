bc9ea98929b49e327c49d4400da5ec6fca3fa6f6
vsparrow <sparrowangels@gmail.com>
2017-05-10 21:11:23 +0000

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,7 +4,15 @@ const request = require("supertest")
 const {app} = require("./../server.js");
 const {Todo} = require("./../models/todo.js")
 
-beforeEach((done)=>{Todo.remove({}).then(()=> done()); }); //wipes todos //start with 0
+const todos = [{text : "First test todo"},{text : "Second test todo"}];
+// beforeEach((done)=>{Todo.remove({}).then(()=> done()); }); //wipes todos //start with 0 //before mod
+beforeEach((done)=>{Todo.remove({}).then(()=> {
+        return Todo.insertMany(todos);
+    }).then(()=>{done();});
+}); //wipes todos //start with 0
+
+
+
 
 describe("POST /todos",()=>{
     it("should create a new todo",(done)=>{
@@ -16,7 +24,7 @@ describe("POST /todos",()=>{
             .end((err,res)=>{
                 if(err){return done(err)}
                 
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done()
@@ -30,7 +38,7 @@ describe("POST /todos",()=>{
         .end((error,res)=>{
             if(error){return done(error)}
             Todo.find().then((todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done()
             }).catch((e)=>{done(e)})
             
@@ -38,4 +46,12 @@ describe("POST /todos",()=>{
 
        
     });
+})
+
+describe("GET /todos",()=>{
+    it("should get all todos",(done)=>{
+        request(app).get("/todos").expect(200).expect((res)=>{
+            expect(res.body.todos.length).toBe(2)
+        }).end(done)
+    })
 })
