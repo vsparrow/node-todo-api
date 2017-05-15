53f812b16174a16bbc9419b108d42573f3752ca1
vsparrow <sparrowangels@gmail.com>
2017-05-15 06:09:15 +0000

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -282,8 +282,6 @@ describe("POST /users",()=>{
 })
 
  
- 
-
 describe("POST /users/login", ()=>{
     it("should login user and return auth token",(done)=>{
         request(app)
@@ -331,4 +329,29 @@ describe("POST /users/login", ()=>{
     
 }); //des
 
+describe("DELETE /users/me/token", ()=>{
+    it("should remove auth token on logout",(done)=>{ 
+       request(app)
+       .delete("/users/me/token")
+       .set("x-auth",users[0].tokens[0].token ) //.set("x-auth").toBe(users[0].tokens.token)
+       .expect(200)
+    //   .expect((res)=>{
+    //     //   User.f
+    //     // console.log(res.body)
+    //         User.findOne({email : users[0].email}).then((user)=>{
+    //             expect(user.tokens.length).toBe(0)
+    //         }).catch((e)=>{done(e)})
+    //   })
+    //   .end(done)
+       .end((err,res)=>{
+           if(err){return done(err)}
+           User.findById(users[0]._id).then((user)=>{
+                expect(user.tokens.length).toBe(0)   
+                done()
+           }).catch((e)=>{done(e)})
+       })
+    });
+});
+    
+ 
 
